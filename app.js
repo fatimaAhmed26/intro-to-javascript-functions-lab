94ae9f585ad5ccc2e34606df6b774b1b94a15659
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-06-18 17:49:59 +0300

    ex2

---
+++ app.js
@@ -0,0 +1,22 @@
+
+/*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+function isAdult(number){
+    if(number <= 18){
+        console.log('Minor')
+    }
+    else{
+        console.log('Adult')
+        return(number)
+    }
+}
+console.log('Exercise 2 Result:', isAdult(21));
